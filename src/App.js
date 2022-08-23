59f4c8c2b792aa0793763343587f54cfea12eb38
lincesites27 <contato@lincesites.com.br>
2022-08-23 15:43:40 -0300

    tentando fazer o mesmo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,44 @@
 import './App.css';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+import Home from './components/Pages/Home';
+import Company from './components/Pages/Company';
+import Contact from './components/Pages/Contact';
+import NewProject from './components/Pages/NewProject';
 
 function App() {
   return (
     
+   <Router>
+    <div></div>
+
+   <Switch>
+    <Route exact path='/'>
+      <Home />
+    </Route>
+    
+    
+    
+    <Route exact path='/company'>
+      <Company />
+    </Route>
+    
 
     
+    <Route exact path='/contact'>
+      <Contact />
+    </Route>
+    
+
+    
+    <Route exact path='/newproject'>
+      <NewProject />
+    </Route>
+   </Switch>
+
+
+   </Router>
+
+
   );
 }
 
