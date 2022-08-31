b20f80ae9b4ab1ff4eafa3bd1a9d1f7487f2f535
lincesites27 <contato@lincesites.com.br>
2022-08-31 14:17:13 -0300

    criando  o useeffect e o fetch

--- src/components/projects/ProjectForm.js
+++ src/components/projects/ProjectForm.js
@@ -1,9 +1,34 @@
+import { useState, useEffect} from 'react'
 import Input from '../form/Input'
 import Select from '../form/Select'
 import SubmitButton from '../form/SubmitButton'
 import styles from './ProjectForm.module.css'
 
 const ProjectForm = ({btnText}) => {
+
+const [categories, setCategories] = useState([])
+
+
+//  O useEffect serve para trazer a requisição uma vez apenas
+useEffect(()=>{
+
+  // O fetch é usado para pegar "GET" os dados do banco de dados
+  // then resp => resp.json transforma os dados em json
+  // then data => setCategories vira data e elas são trocadas
+  
+fetch("http://localhost:5000/categories", {
+  method: "GET",
+  headers:{
+    'Content-Type' : 'aplication/json'
+  }
+}).then((resp)=> resp.json())
+  .then((data)=>{
+    setCategories(data)
+  })
+  .catch((err)=> console.log(err))
+
+}, [])
+
   return (
    <form className={styles.form}>
         <Input 
@@ -20,7 +45,10 @@ const ProjectForm = ({btnText}) => {
         placeholder="Insira o orçamento total" 
         />
 
-        <Select  name="category_id" text="Selecione a categoria"/>
+        <Select  name="category_id" 
+        text="Selecione a categoria"
+        options={categories}
+        />
 
        <SubmitButton text={btnText}/>
 
